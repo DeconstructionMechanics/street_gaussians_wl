6c7c1977546739160df6046d1d2e8c9388e7e9a8
DeconstructionMechanics <easonman2003@hotmail.com>
2024-10-31 15:45:46 +0800

    lidar render alpha blending

--- lib/lidar/gaussian_lidar_renderer.py
+++ lib/lidar/gaussian_lidar_renderer.py
@@ -23,7 +23,7 @@ class GaussianLidarRenderer():
     def __init__(self) -> None:
         pass    
 
-    def render(self, lidar_position: torch.Tensor, beams: torch.Tensor, gaussian: GaussianModel, lidar_range: float=74, angle_range: float=0.999, weight_treshold: float=-1000, step: float=100):
+    def render(self, lidar_position: torch.Tensor, beams: torch.Tensor, gaussian: GaussianModel, lidar_range: float=74, angle_range: float=0.999, weight_treshold: float=-100, step: float=100):
         progress_bar = tqdm(total=beams.shape[0])
 
         covariance = unstrip_symmetric(gaussian.get_covariance()[::step])
@@ -47,27 +47,67 @@ class GaussianLidarRenderer():
 
         return reflect_points, weights
 
-    def render_beams(self, lidar_range: float, angle_range: float, weight_treshold: float, lidar_position: torch.Tensor, beam: torch.Tensor, covariance: torch.Tensor, xyz: torch.Tensor, opacity: torch.Tensor, w_gaussian: float=1, w_distance: float=0):
+    def render_beams(self, lidar_range: float, angle_range: float, weight_treshold: float, lidar_position: torch.Tensor, beam: torch.Tensor, covariance: torch.Tensor, xyz: torch.Tensor, opacity: torch.Tensor, w_gaussian: float=0.01, w_distance: float=0):
         distance = xyz - lidar_position
         line_distance = (distance - torch.einsum('i,j->ij', ((distance @ beam) / (beam @ beam)), beam)).norm(dim=1)
-        intersect_position_parameters = torch.einsum('bi,bij,j->b', distance, covariance, beam) / torch.einsum('i,bij,j->b', beam, covariance, beam)
-        intersect_positions = torch.einsum('b,i->bi', intersect_position_parameters, beam) + lidar_position
-        front_mask = (distance @ beam / (beam.norm() * distance.norm(dim=1)) > angle_range) & (beam.norm() * intersect_position_parameters < lidar_range)
+        intersect_position_coefficient = torch.einsum('bi,bij,j->b', distance, covariance, beam) / torch.einsum('i,bij,j->b', beam, covariance, beam)
+        front_mask = (intersect_position_coefficient > 0) & (distance @ beam / (beam.norm() * distance.norm(dim=1)) > angle_range) & (beam.norm() * intersect_position_coefficient < lidar_range)
+        
+        distance = distance[front_mask]
+        line_distance = line_distance[front_mask]
+        intersect_position_coefficient = intersect_position_coefficient[front_mask]
+        covariance = covariance[front_mask]
+        xyz = xyz[front_mask]
+        opacity = opacity[front_mask]
+        
+        intersect_positions = torch.einsum('b,i->bi', intersect_position_coefficient, beam) + lidar_position
         intersect_mu = intersect_positions - xyz
         
-        weights = torch.log(opacity.reshape(-1)) + torch.log(front_mask) + w_gaussian * (-torch.einsum('bi,bij,bj->b', intersect_mu, covariance, intersect_mu)) - w_distance * torch.log(line_distance)
-        
-        reflect_mask = weights > weight_treshold
-        if torch.any(reflect_mask):
-            reflect_points = intersect_positions[reflect_mask]
-            reflect_point_parameters = intersect_position_parameters[reflect_mask]
-            reflect_weights = weights[reflect_mask]
-            # print(reflect_weights.shape[0], torch.max(reflect_weights), torch.mean(reflect_weights), torch.min(reflect_weights))
-            # print(reflect_point_parameters.shape[0], torch.max(reflect_point_parameters), torch.mean(reflect_point_parameters), torch.min(reflect_point_parameters))
-            response1_index = torch.argmin(reflect_point_parameters + 0.1 * reflect_weights)
+        do_ablend = True
+        if not do_ablend:
+            weights = torch.log(opacity.reshape(-1)) + w_gaussian * (-torch.einsum('bi,bij,bj->b', intersect_mu, covariance, intersect_mu)) - w_distance * torch.log(line_distance)
+            # print(f"weights = {torch.log(opacity.reshape(-1))} + {torch.log(front_mask)} + {w_gaussian * (-torch.einsum('bi,bij,bj->b', intersect_mu, covariance, intersect_mu))}")
+
+            reflect_mask = weights > weight_treshold
+            if torch.any(reflect_mask):
+                reflect_points = intersect_positions[reflect_mask]
+                reflect_point_coefficient = intersect_position_coefficient[reflect_mask]
+                reflect_weights = weights[reflect_mask]
+                # print(reflect_weights.shape[0], torch.max(reflect_weights), torch.mean(reflect_weights), torch.min(reflect_weights))
+                # print(reflect_point_coefficient.shape[0], torch.max(reflect_point_coefficient), torch.mean(reflect_point_coefficient), torch.min(reflect_point_coefficient))
+                response1_index = torch.argmin(0 * reflect_point_coefficient - 1 * reflect_weights)
+
+                if (reflect_points[response1_index] - lidar_position).norm() > lidar_range:
+                    print('[weight] ', reflect_weights[response1_index])
+                    print('[distance] ', reflect_points[response1_index] - lidar_position)
+                    print('[mask] ', front_mask[reflect_mask][response1_index])
+                    print(f'[mask angle] ({distance[reflect_mask][response1_index]} @ {beam} / ({beam.norm()} * {distance[reflect_mask][response1_index].norm()} = ', (distance[reflect_mask][response1_index] @ beam / (beam.norm() * distance[reflect_mask][response1_index].norm())))
+                    print('[mask distance] ', beam.norm() * reflect_point_coefficient[response1_index])
+                    print('[coeff] ', reflect_point_coefficient[response1_index])
+                    raise Exception
+                
+                return reflect_points[response1_index].clone(), float(reflect_weights[response1_index])
+            else:
+                return None, None
             
-            return reflect_points[response1_index].clone(), float(reflect_weights[response1_index])
         else:
+            remaining_opacity = 1
+            intersect_position_coefficient_toreturn = 0
+            weight_sum = 0
+            sort_index = torch.argsort(intersect_position_coefficient)
+            for w, power, coefficient in zip(opacity.reshape(-1)[sort_index], -torch.einsum('bi,bij,bj->b', intersect_mu, covariance, intersect_mu)[sort_index], intersect_position_coefficient[sort_index]):
+                if power < -2.5 or power > 0:
+                    continue
+                alpha = min(0.99, w * torch.exp(power).item())
+                if alpha < 0.004:
+                    continue
+
+                weight_sum
+                intersect_position_coefficient_toreturn += remaining_opacity * alpha * coefficient
+                remaining_opacity *= 1 - alpha
+                
+            if remaining_opacity < 0.5:
+                return (intersect_position_coefficient_toreturn / (1 - remaining_opacity)) * beam + lidar_position, 1 - remaining_opacity
             return None, None
 
 
