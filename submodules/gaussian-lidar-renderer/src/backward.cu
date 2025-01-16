9e37e950a5ec51200c07bec273e2cd839fbedf9a
DeconstructionMechanics <easonman2003@hotmail.com>
2025-01-16 22:49:02 +0800

    backward cuda !test !complete

--- submodules/gaussian-lidar-renderer/src/backward.cu
+++ submodules/gaussian-lidar-renderer/src/backward.cu
@@ -5,6 +5,7 @@
 __device__ void computeFeaturesFromSHBackward(int idx, int deg, int max_coeffs, const float3* means, float3 campos, const float* shs, const bool clamped, float2 dL_dfeature, float3* dL_dmeans, float2* dL_dshs)
 {
 	// Compute intermediate values, as it is done during forward
+	// accumulate(plus) gradients
 	float3 pos = means[idx];
     float3 dir_orig = {
         pos.x - campos.x,
@@ -244,9 +245,74 @@ void backward_shs_cuda(int32_t num_rays, int32_t D, int32_t M, int32_t G,
             }
             bool clamped = *(contribute_clamp + idx * G + iG);
             float2 dL_dfeature = {*(grad_intensityprime + idx * G + iG), *(grad_raydropprime + idx * G + iG)};
-            computeFeaturesFromSHBackward(gaussian_idx, sh_degree, M, (float3*)means3D, ray_o, shs, clamped, dL_dfeature, (float3*)grad_means_from_shs, (float2*)grad_shs_from_shs); // TODO clamped
+            computeFeaturesFromSHBackward(gaussian_idx, D, M, (float3*)means3D, ray_o, shs, clamped, dL_dfeature, (float3*)grad_means_from_shs, (float2*)grad_shs_from_shs);
         }
     }); 
 }
 
 
+void backward_trace_cuda(int32_t num_rays, int32_t D, int32_t M, int32_t G,
+    int32_t* contribute_gid, float* contribute_T, bool* contribute_clamp,
+	float* contribute_tprime, float* contribute_intensityprime, float* contribute_raydropprime,
+    float* weights, float* tvalues, float* intensity, float* raydrop,
+    float3* means3D, float* covs3D, float* opacity, float* shs,
+	float3* rays_o, float3* rays_d,
+	float* grad_up_weights, float* grad_up_tvalues, float* grad_up_intensity, float* grad_up_raydrop,
+	float3* grad_means3D, float* grad_covs3D, float* grad_opacity, float2* grad_shs){
+		// covs3D stripped to 6
+    thrust::for_each(thrust::device,
+        thrust::make_counting_iterator<int32_t>(0),
+        thrust::make_counting_iterator<int32_t>(num_rays),
+        [D, M, G,
+		contribute_gid, contribute_T, contribute_clamp,
+		contribute_tprime, contribute_intensityprime, contribute_raydropprime,
+		weights, tvalues, intensity, raydrop,
+        means3D, covs3D, opacity, shs,
+		rays_o, rays_d,
+		grad_up_weights, grad_up_tvalues, grad_up_intensity, grad_up_raydrop,
+		grad_means3D, grad_covs3D, grad_opacity, grad_shs] __device__(int32_t ray_idx){
+        
+        float3 ray_o = rays_o[ray_idx];
+		float3 ray_d = rays_d[ray_idx];
+		float ray_weight = weights[ray_idx];
+		float ray_grad_up_tvalue = grad_up_tvalues[ray_idx];
+		float ray_grad_up_intensity = grad_up_intensity[ray_idx];
+		float ray_grad_up_raydrop = grad_up_raydrop[ray_idx];
+		
+        for (int32_t iG = 0; iG < G; iG++){
+            int32_t gaussian_idx = *(contribute_gid + idx * G + iG);
+            if (gaussian_idx < 0){
+                continue;
+            }
+			float T = *(contribute_T + idx * G + iG);
+            bool clamp = *(contribute_clamp + idx * G + iG);
+			float tprime = *(contribute_tprime + idx * G + iG);
+			float intensityprime = *(contribute_intensityprime + idx * G + iG);
+			float raydropprime = *(contribute_raydropprime + idx * G + iG);
+
+			float3 pos = {
+							ray_o.x + tprime * ray_d.x,
+							ray_o.y + tprime * ray_d.y,
+							ray_o.z + tprime * ray_d.z,
+			};
+			float power = gaussian_fn(means3D[gaussian_idx], pos, covs3D + gaussian_idx * 6);
+			float alpha = opacity[gaussian_idx] * __expf(power);
+
+			// feature -> featureprime
+			float dfeature_dfeatureprime = T * alpha;
+			float grad_tprime = dfeature_dfeatureprime * ray_grad_up_tvalue / fmaxf(ray_weight, 0.0001f);
+			float grad_intensityprime = dfeature_dfeatureprime * ray_grad_up_intensity;
+			float grad_raydropprime = dfeature_dfeatureprime * ray_grad_up_raydrop;
+
+			// tprime -> means3D
+
+			// tprime -> covs3D
+
+			// opacity
+
+			// featureprime -> sh
+            float2 grad_feature = {grad_intensityprime, grad_raydropprime};
+            computeFeaturesFromSHBackward(gaussian_idx, D, M, means3D, ray_o, shs, clamp, grad_feature, grad_means3D, grad_shs);
+        }
+    }); 
+}
